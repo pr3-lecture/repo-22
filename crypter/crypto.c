219433d7d2e78036fe11d889b65b5e8e39917431
David Nadoba <dnadoba@gmail.com>
2017-12-29 22:58:44 +0100

    Fix wrong error code

--- crypter/crypto.c
+++ crypter/crypto.c
@@ -24,13 +24,13 @@ int checkXORKey(KEY key) {
     for(int i = 0; i < key_length; i++) {
         char key_char = key.chars[i];
         if(key_char < '@' || key_char > '_') {
-            return E_CYPHER_ILLEGAL_CHAR;
+            return E_KEY_ILLEGAL_CHAR;
         }
     }
     return 0;
 }
 
-int xor(KEY key, const char *input, char* output, char minValidInputChar, char maxValidInputChar) {
+int xor(KEY key, const char *input, char* output, char minValidInputChar, char maxValidInputChar, int invalidInputCharErrorCode) {
     int error = checkXORKey(key);
     if(error != 0) {
         return error;
@@ -43,7 +43,7 @@ int xor(KEY key, const char *input, char* output, char minValidInputChar, char m
         //get and validate current input char
         char input_char = input[i];
         if(input_char < minValidInputChar || input_char > maxValidInputChar) {
-            return E_MESSAGE_ILLEGAL_CHAR;
+            return invalidInputCharErrorCode;
         }
         int input_value = valueFromChar(input_char);
         
@@ -61,11 +61,11 @@ int xor(KEY key, const char *input, char* output, char minValidInputChar, char m
 }
 
 int encrypt(KEY key, const char *input, char* output) {
-    return xor(key, input, output, 'A', 'Z');
+    return xor(key, input, output, 'A', 'Z', E_MESSAGE_ILLEGAL_CHAR);
 }
 
 int decrypt(KEY key, const char *input, char* output) {
-    return xor(key, input, output, '@', '_');
+    return xor(key, input, output, '@', '_', E_CYPHER_ILLEGAL_CHAR);
 }
 
 char* cryptoErrorString(int error) {
